dd5d9b23e7dedced26e2867e6c02a202dfdfd8ed
kayciec <k.cahill1@ardencahillacademy.com>
2021-10-26 13:45:11 +0000

    changed platform

--- projects/platformer/js/init/platform.js
+++ projects/platformer/js/init/platform.js
@@ -40,7 +40,7 @@
         createPlatform(0,410,0.8,0.7);
         createPlatform(100,540,0.9,0.7);
         createPlatform(450,0,0.05,20,0.7,false);
-        createPlatform(600,560,0.8,0.7);
+        createPlatform(600,550,0.8,0.7);
         createPlatform(450,430,0.6,0.7);
         createPlatform(600,290,0.8,0.7);
         createPlatform(450,150,0.6,0.7,true);
