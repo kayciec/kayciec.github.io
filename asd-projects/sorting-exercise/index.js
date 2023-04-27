70a49d7cc3348ee63899581a689e2c6457282ebe
kayciec <k.cahill1@ardencahillacademy.com>
2023-04-27 14:37:02 +0000

    done

--- asd-projects/sorting-exercise/index.js
+++ asd-projects/sorting-exercise/index.js
@@ -35,10 +35,10 @@ async function quickSort(array, left, right) {
     if ((right - left) > 0) {
         var index = await partition(array, left, right)
         if (left < (index - 1)) {
-            await quicksort(array, left, index - 1)
+            await quickSort(array, left, index - 1)
         }
         if (index < right) {
-            await quicksort(array, index, right)
+            await quickSort(array, index, right)
         }
     }
     else {
